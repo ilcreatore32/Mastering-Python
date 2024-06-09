64a8a6df9ac059967ffc75e127d0759e715326e2
Weishler Berman <52388760+ilcreatore32@users.noreply.github.com>
2024-06-09 20:45:50 +0000

    Get Started with Hangman Game

---
+++ Hangman Game/hangman.py
@@ -0,0 +1,8 @@
+from random import choice
+
+def select_word():
+    with open("./words.txt", mode="r") as words:
+        word_list = words.readlines()
+    return choice(word_list).strip()
+
+select_word()
